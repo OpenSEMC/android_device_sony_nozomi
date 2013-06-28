cdff7aed0e813bbd7e4164f52c7a692d6970f510
RaymanFX <raymanfx@gmail.com>
2013-06-28 14:52:09 +0200

    initial CFX device tree changes

--- cfx.mk
+++ cfx.mk
@@ -16,11 +16,11 @@
 
 $(call inherit-product, device/sony/nozomi/full_nozomi.mk)
 
-# Inherit CM common GSM stuff.
-$(call inherit-product, vendor/cm/config/gsm.mk)
+# Inherit CFX common GSM stuff.
+$(call inherit-product, vendor/cfx/config/gsm.mk)
 
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+# Enhanced NFC - not yet present in CFX
+#$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 # Inherit CM common Phone stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
@@ -29,10 +29,10 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
-# Inherit CM common Phone stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit CFX common Phone stuff.
+$(call inherit-product, vendor/cfx/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=LT26i_1257-5499 BUILD_FINGERPRINT=SEMC/LT26i_1257-5499/LT26i:4.0.4/6.1.A.2.50/zfd_zw:user/release-keys PRIVATE_BUILD_DESC="LT26i-user 4.0.4 6.1.A.2.50 zfd_zw test-keys"
 
-PRODUCT_NAME := cm_nozomi
+PRODUCT_NAME := cfx_nozomi
 PRODUCT_DEVICE := nozomi
