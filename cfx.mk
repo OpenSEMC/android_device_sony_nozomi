871e50889fe3132d753a32085f54f361f8edd265
Christopher N. Hesse <raymanfx@gmail.com>
2013-06-29 10:55:57 +0200

    cfx: do not override device props here

--- cfx.mk
+++ cfx.mk
@@ -33,6 +33,3 @@ TARGET_SCREEN_WIDTH := 720
 $(call inherit-product, vendor/cfx/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=LT26i_1257-5499 BUILD_FINGERPRINT=SEMC/LT26i_1257-5499/LT26i:4.0.4/6.1.A.2.50/zfd_zw:user/release-keys PRIVATE_BUILD_DESC="LT26i-user 4.0.4 6.1.A.2.50 zfd_zw test-keys"
-
-PRODUCT_NAME := cfx_nozomi
-PRODUCT_DEVICE := nozomi
